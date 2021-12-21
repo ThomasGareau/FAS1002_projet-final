7a380de009029e7752c79df11ec09c7eda2e1030
Thomas Gareau Paquette <thomas.gareau.paquette@umontreal.ca>
2021-12-21 12:00:08 -0500

    Retiré données "world"

--- data/telecharger_raw.R
+++ data/telecharger_raw.R
@@ -60,7 +60,8 @@ rm(life_exp_raw)
 ### Mise à jour lorsque month(pop_raw_date) != month
 
 ifelse(month(life_exp_date) == month, "Les données sur l'espérance de vie de Gapminder ont été colligées dans le dernier mois (il y a moins de 31 jours)", download.file("https://docs.google.com/spreadsheets/d/11mulzUH3_cueq-V9D5KIlo9oHE9YYZrUSeVyCin7_rM/export?format=xlsx", destfile = "data/raw/life_exp_raw.xlsx"))
-rm(life_exp_date)
+rm(life_exp_date,
+   month)
 
 
 ## Ouvrir translator ISO - Pays - Continent
@@ -75,20 +76,17 @@ vaccination_raw <- read_csv("data/raw/vaccination_raw.csv")
 
 ### 4.2 Data Population 
 
-world_pop_raw <- read_excel("data/raw/population_raw.xlsx", sheet = 2)
 regions_pop_raw <- read_excel("data/raw/population_raw.xlsx", sheet = 3)
 countries_pop_raw <- read_excel("data/raw/population_raw.xlsx", sheet = 4)
 
 ### 4.3 Data PIB par habitant
 
-world_gdpc_raw <- read_excel("data/raw/gdp_capita_raw.xlsx", sheet = 2)
 regions_gdpc_raw <- read_excel("data/raw/gdp_capita_raw.xlsx", sheet = 3)
 countries_gdpc_raw <- read_excel("data/raw/gdp_capita_raw.xlsx", sheet = 4)
 
 
 ### 4.4 Data espérance de vie
 
-world_lifeexp_raw <- read_excel("data/raw/life_exp_raw.xlsx", sheet = 2)
 regions_lifeexp_raw <- read_excel("data/raw/life_exp_raw.xlsx", sheet = 3)
 countries_lifeexp_raw <- read_excel("data/raw/life_exp_raw.xlsx", sheet = 4)
 

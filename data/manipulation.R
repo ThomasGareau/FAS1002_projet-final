202fac63495217ea8ec6129f87bff4c42b94b01a
Thomas Gareau Paquette <thomas.gareau.paquette@umontreal.ca>
2021-12-21 15:22:24 -0500

    Ajout des manipulations

--- data/manipulation.R
+++ data/manipulation.R
@@ -1,24 +1,18 @@
 ########## Manipulation des données et merge #########
 
-# Packages
+########### Packages #############
 
 require(stringr)
 require(tidyverse)
+library(extras)
 
 # 1. Données vaccination
 
 
-## Retrait des données inutiles
+### 1.1 Retrait des données inutiles
 
 vaccination_raw <- vaccination_raw %>%
-    select(c(-daily_people_vaccinated, -daily_vaccinations, -daily_vaccinations_per_million, -daily_people_vaccinated_per_hundred, -daily_vaccinations_raw))
-
-
-### 1.1 Créer une banque de données pour les données mondiales
-
-world_vaccination <- vaccination_raw %>%
-    filter(str_detect(location, "World"))
-
+    select(-daily_vaccinations_raw)
 
 
 ### 1.2 Créer une banque de données pour les continents
@@ -46,29 +40,32 @@ continent_vaccination <- vaccination_raw %>%
      rename(continent = location)
  
  
- ### Pour avoir le temps en semaines
+ #### Pour avoir le temps en semaines
  
+
 continent_vaccination_semaine <- continent_vaccination %>% 
      mutate(time = format(date, "%Y-%m-%U")) %>% 
     pivot_wider(names_from= continent,
-                values_from=people_vaccinated)
+                values_from=daily_vaccinations)
 
 
-### Pour avoir le nombre de vaccinations totales par semaine en fonction du continent
+
+
+#### Pour avoir le chiffre représentant la moyenne de la vaccination hebdomadaire en fonction du continent
 
 
 continent_vaccination_semaine <- continent_vaccination_semaine %>% group_by(time) %>%
-    summarize(Africa = sum(Africa, na.rm = TRUE),
-              Europe = sum(Europe, na.rm = TRUE),
-              Asia = sum(Asia, na.rm = TRUE),
-              Americas = sum(Americas, na.rm = TRUE),
-              Oceania = sum(Oceania, na.rm = TRUE),
-              total = sum(Oceania + Africa + Americas + Asia + Europe + Africa, na.rm = TRUE))
+    summarize(Africa = mean(Africa, na.rm = TRUE),
+              Europe = mean(Europe, na.rm = TRUE),
+              Asia = mean(Asia, na.rm = TRUE),
+              Americas = mean(Americas, na.rm = TRUE),
+              Oceania = mean(Oceania, na.rm = TRUE),
+              total = mean(Oceania + Africa + Americas + Asia + Europe + Africa, na.rm = TRUE))
 
 
 
 
- #### Pour avoir le nombre de vaccinations totales par semaine en fonction du continent enn pourcentage
+#### Pour avoir le nombre de vaccinations hebodmadaires par semaine en fonction du continent en pourcentage
 
 continent_vaccination_semaine = continent_vaccination_semaine %>%
      group_by(time) %>%
@@ -81,51 +78,103 @@ continent_vaccination_semaine = continent_vaccination_semaine %>%
 
 
 
+#### Pour avoir le nombre de vaccination totale par continent pour 2021
 
-### 1.3 Créer une banque de données à part pour les pays
+
+continent_vaccination_annee <- continent_vaccination %>%
+    select(-c(daily_people_vaccinated_per_hundred, daily_vaccinations_per_million, daily_people_vaccinated)) %>%
+    fill(c(date, continent, total_vaccinations), .direction = "downup") %>%
+    fill(c(date, continent, people_vaccinated), .direction = "downup") %>%
+    fill(c(date, continent, people_fully_vaccinated), .direction = "downup") %>%
+    fill(c(date, continent, daily_vaccinations), .direction = "downup") %>%
+    fill(c(date, continent, people_vaccinated_per_hundred), .direction = "downup") %>%
+    fill(c(date, continent, total_boosters_per_hundred), .direction = "downup") %>%
+    fill(c(date, continent, people_fully_vaccinated_per_hundred), .direction = "downup") %>%
+    fill(c(date, continent, total_vaccinations_per_hundred), .direction = "downup") %>%
+    subset(date == Sys.Date() -7) %>%
+    group_by(continent) %>%
+    summarize_if(is.numeric, .funs = sum, na.rm = TRUE)
+
+
+
+continent_vaccination_annee <- continent_vaccination_annee %>%
+    mutate(continent = str_replace_all(continent, "Americas", "The Americas")) %>%
+    subset(continent != "Oceania")
+
+
+
+
+### 1.3 Créer une banque de données pour les pays sur la vaccination quotidienne
 
 
 country_vaccination <- vaccination_raw %>%
     filter(!str_detect(iso_code, "OWID")) %>%
     rename(country = location)
 
-rm(vaccination_raw)
-
 #### Pour avoir le temps en semaines
 
 country_vaccination_semaine <- country_vaccination %>% 
     mutate(time = format(date, "%Y-%m-%U")) %>% 
     pivot_wider(names_from = country,
-                values_from=people_vaccinated)
+                values_from=daily_vaccinations)
 
 
-#### Pour avoir le nombre de vaccinations totales par semaine en fonction du continent
+
+#### Pour avoir le nombre de vaccinations moyennes par semaine en fonction du pays
 
 
 country_vaccination_semaine <- country_vaccination_semaine %>% group_by(time) %>%
     summarize_if(is.numeric, .funs = sum, na.rm = TRUE)
 
 
+#### Pour avoir les données de vaccination les plus récentes (certains pays partagent leurs informations aux semaines, d'où le -7)
 
-## 2. Gampminder
+vaccination_today <- vaccination_raw %>%
+    select(-c(daily_people_vaccinated_per_hundred, daily_vaccinations_per_million, daily_people_vaccinated)) %>%
+    rename(country = location) %>%
+    fill(c(date, country, total_vaccinations), .direction = "downup") %>%
+    fill(c(date, country, people_vaccinated), .direction = "downup") %>%
+    fill(c(date, country, people_fully_vaccinated), .direction = "downup") %>%
+    fill(c(date, country, daily_vaccinations), .direction = "downup") %>%
+    fill(c(date, country, people_vaccinated_per_hundred), .direction = "downup") %>%
+    fill(c(date, country, total_boosters_per_hundred), .direction = "downup") %>%
+    fill(c(date, country, people_fully_vaccinated_per_hundred), .direction = "downup") %>%
+    fill(c(date, country, total_vaccinations_per_hundred), .direction = "downup") %>%
+    subset(date == Sys.Date() -7) %>%
+    group_by(country) %>%
+    summarize_if(is.numeric, .funs = sum, na.rm = TRUE)
+
+
+rm(continent_vaccination,
+   vaccination_raw,
+   country_vaccination)
+
+
+
+
+
+
+
+######## 2. Gampminder #######
 
 ### 2.1 Population mondiale
 
-world_pop <- world_pop_raw %>%
-    rename(world = name)
-
-rm(world_pop_raw)
-
 continent_pop <- regions_pop_raw %>%
-    rename(continent = name)
+    subset(time == 2021) %>%
+    pivot_wider(names_from=time, values_from=Population) %>%
+    rename("population_2021" = "2021",
+           "continent" = "name") %>%
+    select(-geo)
 
-rm(regions_pop_raw)
+
 
 
 country_pop <- countries_pop_raw %>%
-    rename(country = name)
+    subset(time == 2021) %>%
+    pivot_wider(names_from=time, values_from=Population) %>%
+    rename("population_2021" = "2021",
+           "country" = "name")  # Nous garderons ce "geo"
 
-rm(countries_pop_raw)
 
 
 
@@ -133,45 +182,36 @@ rm(countries_pop_raw)
 
 ### 2.2 PIB mondial
 
-world_gdpc  <- world_gdpc_raw %>%
-    rename(world = name)
-
-rm(world_gdpc_raw)
-
-
 continent_gdpc <- regions_gdpc_raw %>%
-    rename(continent = name)
+    subset(time == 2021) %>% 
+    pivot_wider(names_from=time, values_from= "GDP per capita growth (%)") 
 
-rm(regions_gdpc_raw)
+continent_gdpc <- continent_gdpc %>%
+    rename("growth_2021" = "2021",
+           "continent" = "name") %>%
+    select(-geo)
 
 country_gdpc <- countries_gdpc_raw %>% 
     rename("GDP_capita_growth" = "Annual percentage growth rate of GDP per capita based on constant interantional dollars. Aggregates are based on PPP, constant 2017 U.S. dollars. GDP per capita is gross domestic product divided by population. GDP at purchaser's prices is the sum of gross value added by all resident producers in the economy plus any product taxes and minus any subsidies not included in the value of the products. It is calculated without making deductions for depreciation of fabricated assets or for depletion and degradation of natural resources.")
-
-rm(countries_gdpc_raw)
-
 
 country_gdpc <- country_gdpc %>%
     subset(time == 2021) %>%
     pivot_wider(names_from=time, values_from=GDP_capita_growth) %>%
     rename("growth_2021" = "2021",
-           "country" = "name")
-
-
-
+           "country" = "name") %>%
+    select(-geo)
 
 
 ### 2.3 Espérance de vie
 
-world_lifeexp  <- world_lifeexp_raw %>% 
-    rename(world = name)
 
-rm(world_lifeexp_raw)
+continent_lifeexp <- regions_lifeexp_raw %>%
+    subset(time == 2021) %>%
+    pivot_wider(names_from=time, values_from="Life expectancy") %>%
+    rename("lifeeexp_2021" = "2021",
+           "continent" = "name") %>%
+    select(-geo)
 
-
-continent_lifeexp <- regions_lifeexp_raw %>% 
-    rename(continent = name)
-
-rm(regions_lifeexp_raw)
 
 
 country_lifeexp <- countries_lifeexp_raw
@@ -179,49 +219,93 @@ country_lifeexp <- countries_lifeexp_raw
     subset(time == 2021) %>%
     pivot_wider(names_from=time, values_from="Life expectancy") %>%
     rename("lifeeexp_2021" = "2021",
-           "country" = "name")
+           "country" = "name") %>%
+        select(-geo)
+
+
+rm(countries_lifeexp_raw,
+countries_gdpc_raw,
+countries_pop_raw)
+
+
+rm(regions_lifeexp_raw,
+regions_gdpc_raw,
+regions_pop_raw)
 
 
 
-rm(countries_lifeexp_raw)
+
+
+#####  Merge des bases de données par pays (vérifier les 100 habitants) #####
+
+df_country <- right_join(vaccination_today, country_pop, by="country")
+
+df_country <- right_join(country_gdpc, df_country, by="country")
+
+df_country <- right_join(country_lifeexp, df_country, by="country")
 
 
 
-### Pour vérifier la mortalité par 100 habitants
+rm(vaccination_today,
+country_pop,
+country_gdpc,
+country_lifeexp)
 
-country_pop <- country_pop %>%
-    subset(time == 2021) %>%
-    pivot_wider(names_from=time, values_from=Population) %>%
-    rename("population_2020" = "2020",
-           "population_2021" = "2021",
-           "country" = "name")
+#####  Merge des bases de données par continent (vérifier les 100 habitants) #####
 
+df_continent <- right_join(continent_vaccination_annee, continent_pop, by="continent")
 
-### Pour avoir les cas de covid par année 
-country_vaccination_annee <- country_vaccination %>%
-    mutate(time = format(date, "%Y")) 
+df_continent <- right_join(continent_gdpc, df_continent, by="continent")
 
-country_vaccination_annee <- country_vaccination_annee %>%
-    drop_na(total_vaccinations) %>% subset(time == 2021)
-
-country_vaccination_annee <- country_vaccination_annee %>% group_by(country, time) %>%
-    summarize_if(is.numeric, .funs = sum)
+df_continent <- right_join(continent_lifeexp, df_continent, by="continent")
 
 
-#####  Merge des deux bases de données
+rm(continent_vaccination_annee,
+   continent_pop,
+   continent_gdpc,
+   continent_lifeexp)
 
-vaccination_pop <- right_join(country_vaccination_annee, country_pop, by="country")
+
+##### Merge des bases de données par semaine ####
+
+df_semaine <- right_join(continent_vaccination_semaine, country_vaccination_semaine, by="time")
+
+rm(continent_vaccination_semaine,
+   country_vaccination_semaine)
 
 
-### Deuxième critère : créer la variable continent
+############ MANIPULATIONS OBLIGATOIRES ############
+
+### Premier critère : créer une nouvelle variable pour les données par pays
 
 iso_translator <- iso_translator %>%
     select(Continent_Name, Three_Letter_Country_Code)
 
+iso_translator <- iso_translator %>%
+    rename(geo = Three_Letter_Country_Code) %>%
+    mutate(geo = tolower(geo))
+
+df_country <- full_join(iso_translator, df_country, by="geo") %>% 
+    drop_na("total_vaccinations")
 
 
-########### Sauvegardes
+rm(iso_translator)
 
-# write.csv(df_country_merged, "data/processed/df_country_merged.csv") 
+
+
+### Second critère : vérifier la validité des valeurs de la variable "total_vaccinations_per_hundred"
+
+df_country <- df_country %>%
+    mutate(verficiation_vaccination_par_100 = NA,
+           verficiation_vaccination_par_100 = (total_vaccinations/population_2021*100)) %>%
+    relocate(verficiation_vaccination_par_100, .before = total_vaccinations_per_hundred) ##### Validité vérifiée
+
+
+
+########### Sauvegardes #########
+
+write.csv(df_semaine, "data/processed/daily_vaccination_semaine_clean.csv")
+write.csv(df_continent, "data/processed/continent_clean.csv") 
+write.csv(df_country, "data/processed/country_clean.csv")
 
 
